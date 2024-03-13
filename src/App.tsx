29e30c2bea55c2ae8d0a2653b438502b1cb4bb26
Mateus F dev <mateusfdev@outlook.com>
2024-03-13 13:25:24 -0300

    create routes 🍿🧂🍿

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,16 @@
 import { ThemeProvider } from 'styled-components'
-import { Button } from "./components/button";
 import { defaultTheme } from './styles/themes/default';
 import { GlobalStyle } from './styles/global';
+import { Router } from './Router';
+import { BrowserRouter } from  'react-router-dom'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-
-      <Button variant="primary"/>
-      <Button variant="secondary"/>
-      <Button variant="dark"/>
-      <Button variant="sucsses"/>
-      <Button variant="danger"/>
-      <Button />
-      <GlobalStyle />
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+        <GlobalStyle />
     </ThemeProvider>
   )
   
--- src/components/button.tsx
+++
@@ -1,9 +0,0 @@
-import { ButtonContainer, ButtonVariant } from './button.styles.ts'
-
-interface ButtonProps {
-  variant?: ButtonVariant;
-}
-
-export function Button({ variant = 'primary' }: ButtonProps) {
-  return <ButtonContainer variant={variant}>Enviar</ButtonContainer>
-}
